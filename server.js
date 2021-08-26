9d1e4961023ac628a025c5369d5d7729719bfa45
hanliu49 <hanliu49@hotmail.com>
2021-08-26 19:31:47 +1000

    back-end test project starts

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+/* sign in */
+app.post("/signin", (req, res) => {
+  res.send("in 200");
+});
+
+/* sign up*/
+app.post("/signup", (req, res) => {
+  res.send("up 200");
+});
+
+/* litsen on port 5000 */
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
