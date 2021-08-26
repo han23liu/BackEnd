be41e174abfc6c205644c7d1a2f7ee2990f83943
Andrew <h23liu@gmail.com>
2021-08-26 22:39:19 +1000

    update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
-var cors = require("cors");
+//var cors = require("cors");
 
 const app = express();
-app.use(cors());
+//app.use(cors());
 const localPort = 5000;
 
 /* sign in */
