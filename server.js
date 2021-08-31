c12113e2648d4d9c389a0426b71dad5170c81e0e
Andrew <h23liu@gmail.com>
2021-08-31 11:48:43 +1000

    test

--- server.js
+++ server.js
@@ -5,32 +5,30 @@ const app = express();
 app.use(
   cors({
     origin: "https://frontend-test13579.herokuapp.com/",
-    methods: ["GET", "POST"],
   })
 );
 
 /* sign in */
 app.post("/signin", (req, res) => {
+  console.log("singin post");
   res.send("in 200");
 });
 
 app.get("/signin", (req, res) => {
+  console.log("singin get");
   res.send("signin page");
 });
 
 /* sign up*/
 app.post("/signup", (req, res) => {
+  console.log("singup post");
   res.send("up 200");
 });
 
 app.get("/signup", (req, res) => {
+  console.log("singup get");
   res.send("signup page");
 });
 
-/* home page*/
-app.get("/", (req, res) => {
-  res.send("home page");
-});
-
-/* litsen on port 5000 */
-app.listen(process.env.PORT || 5000);
+/* litsen on port process.env.PORT || 5000 */
+app.listen(5000);
